d52eefad19435d89752c8314bb32a61dc5900e39
Mickaël <mickael_fournier@hotmail.fr>
2015-04-09 15:16:40 +0200

    Delete serverHyun

--- serverTestHyun/sql/data.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT INTO user_types (type) VALUES ('user');
-INSERT INTO user_types (type) VALUES ('robot');
-	
-INSERT INTO users (login,pw,user_type) VALUES ('cosc','123','user'); 
-INSERT INTO users (login,pw,user_type) VALUES ('vignesn','123','user'); 
-INSERT INTO users (login,pw,user_type) VALUES ('mjk','123','user'); 
-
-INSERT INTO users (login,pw,user_type) VALUES ('robair1','123','robot');
